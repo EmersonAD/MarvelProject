709b6d2656cba8b31662a6e87c27cf38b1a5d4e1
Emerson Soares <souzaemerson@brq.com>
2022-06-02 20:03:41 -0300

    [US]#001 - Layouts and manifest config

--- app/src/main/java/com/souzaemerson/marvelproject/MainActivity.kt
+++ app/src/main/java/com/souzaemerson/marvelproject/MainActivity.kt
@@ -2,10 +2,16 @@ package com.souzaemerson.marvelproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
