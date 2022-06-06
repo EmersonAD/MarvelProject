e4c9e18e2b4ea1d69696061ba380b9bffb7c1714
Emerson Soares <souzaemerson@brq.com>
2022-06-05 22:24:01 -0300

    Melhorias na character_item e modificação do theme night

--- app/src/main/java/com/souzaemerson/marvelproject/MainActivity.kt
+++ app/src/main/java/com/souzaemerson/marvelproject/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
     }
 }
