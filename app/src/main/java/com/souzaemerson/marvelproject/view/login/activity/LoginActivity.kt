3b1994cb11b282839c0b23738222155716f68de2
Emerson Soares <souzaemerson@brq.com>
2022-07-25 14:01:56 -0300

    Criação da custom view para botão de loading

--- app/src/main/java/com/souzaemerson/marvelproject/view/login/activity/LoginActivity.kt
+++ app/src/main/java/com/souzaemerson/marvelproject/view/login/activity/LoginActivity.kt
@@ -1,18 +1,19 @@
 package com.souzaemerson.marvelproject.view.login.activity
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.souzaemerson.marvelproject.R
+import androidx.appcompat.app.AppCompatActivity
 import com.souzaemerson.marvelproject.databinding.ActivityLoginBinding
-import com.souzaemerson.marvelproject.databinding.ActivityRegisterBinding
-import com.souzaemerson.marvelproject.view.login.register.activity.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            binding.loginButton.progress(true)
+        }
     }
 }
